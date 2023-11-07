2a7f2a67e6ac8dfbb0939bef5a14215e8c8dbd65
Hebearo <asd094198@gmail.com>
2023-11-07 10:19:56 +0800

    save

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ async def on_message(message):
             await message.channel.send('成功上傳漫畫網站')
         elif(message.content.startswith('!#ms')):
             content = {}
-            manga_dict = await manga_state_class.get_manga_dict(channel_id)
+            manga_dict = await manga_state_class.get_channelinfo_dict(channel_id)
             for web in manga_dict['manga'].keys():
                 content[web] = manga_dict['manga'][web]  
             text = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S 羅列漫畫\n")
@@ -59,16 +59,14 @@ async def on_message(message):
             suc = await chicken_state_class.add_item(channel=message.channel , people_list=people_name)
             await message.channel.send('成功上傳:' + ','.join(suc['suc']))
             await message.channel.send('以下失敗:' + ','.join(suc['non']))
-path_log_book = './data/log_book/manga_output.md'
-def output_to_log_book(typee='mna',text='none'):
-    #typee = 'mna'/'usr'
-    timing = datetime.datetime.now()  
-    timing_format = timing.strftime("%Y_%m_%d@%H_%M_%S@{}@".format(typee))
-    with open( path_log_book,'a+',encoding='utf-8') as output:
-        output.write( timing_format)
-        output.write( text + '\n' )
-    pass
+        elif(message.content.startswith('!#cs')):
+            content = {}
+            chicken_dict = await chicken_state_class.get_channelinfo_dict(channel_id)      
+            text = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S 告知有誰的雞湯\n")
 
+            for people in chicken_dict['soup'].keys():
+                text = text + people +' 有 ' + str(len(chicken_dict['soup'][people])) +' 碗 雞湯 \n'
+            await message.channel.send(text)
 
 
 def _deal_with_text(update_list = [manga_updated]):
@@ -82,15 +80,15 @@ async def threading_manga_state():
     global manga_state_class
     await manga_state_class.update_json()
 
-    output_to_log_book('ini','suc@start_update')
+    await manga_state_class.output_logbook('ini','suc@start_update')
     manga_channel_id_dict = await manga_state_class.get_channels_dict()
     for channel_id in manga_channel_id_dict:
         if(channel_id=='demo' ):
             continue
         
-        output_to_log_book('ini','beg@{}'.format(channel_id))
+        await manga_state_class.output_logbook('ini','beg@{}'.format(channel_id))
         update_list =  []
-        channel_dict = await manga_state_class.get_manga_dict(channel_id)
+        channel_dict = await manga_state_class.get_channelinfo_dict(channel_id)
 
         for web in channel_dict['manga'].keys():
             try:
@@ -99,9 +97,9 @@ async def threading_manga_state():
                 manga_class= check_manga_state(web,current_value)
             except:
                 print('error occur in opening')
-                output_to_log_book('mnm','err'+'@{}'.format(web))
+                await manga_state_class.output_logbook('mnm','err'+'@{}'.format(web))
                 continue
-            output_to_log_book('mnm',manga_class._state+'@{}'.format(web))
+            await manga_state_class.output_logbook('mnm',manga_class._state+'@{}'.format(web))
             if( manga_class._state =='updated'):
                 update_list.append( manga_class )
             else:
@@ -111,7 +109,7 @@ async def threading_manga_state():
             #如果有更新
             text = _deal_with_text(update_list)
             await client.get_channel( int(channel_id) ).send(text)
-            output_to_log_book('usr','suc@'+channel_id)
+            await manga_state_class.output_logbook('usr','suc@'+channel_id)
             for updates in update_list:
                 print( updates._web + '::'+updates._words[1])
                 await manga_state_class.update_manga_info(channel_id , updates)
@@ -126,6 +124,22 @@ async def threading_manga_state():
 async def threading_soup_sending():
     global chicken_state_class
     await chicken_state_class.update_json()
+    await chicken_state_class.output_logbook('ini','suc@start_update')
+
+    chicken_channel_dict = await chicken_state_class.get_channels_dict()
+    for channel_id in chicken_channel_dict.keys():
+        if(channel_id=='demo' ):
+            continue
+        
+        await chicken_state_class.output_logbook('ini','beg@{}'.format(channel_id))
+        update_list =  []
+        channel_dict = await chicken_state_class.get_channelinfo_dict(channel_id)
+        if( len(channel_dict['soup']) == 0):
+            continue
+        else:
+            people_name = random.choice( list( channel_dict['soup'].keys() ) )
+            people_speech = random.choice(  channel_dict['soup'][people_name] )
+            await client.get_channel( int(channel_id) ).send( people_name + ' : ' + people_speech)
 
 
 
@@ -140,4 +154,5 @@ async def on_ready():
     manga_state_class = manga_state(path = 'pkg/manga_state.json') 
     
     threading_manga_state.start()
-client.run("MTE3MDIyMjExODQ1NDE4NjAzNA.G6meI7.4lyEH_pphT_tKGRBYdopxfqwqOhE8i2Ka7OdVo")
+    threading_soup_sending.start()
+client.run("MTE3MDIyMjExODQ1NDE4NjAzNA.GRpDx9.83l3xsXwmdS32Wlz1Fq3lzrMbqQIxQdvwQUQcA")
--- pkg/state_relate/state_model.py
+++ pkg/state_relate/state_model.py
@@ -5,6 +5,7 @@ import json
 import copy
 import discord.channel as dc_channel
 from pkg.manga_pkg import *
+import datetime
 '''
 定義一個聊天室為一個channel
 '''
@@ -17,6 +18,10 @@ class fundamental_state(ABC):
     async def add_channel(self,channel:dc_channel,label_name = 'Null' ):
         self._state_dict[str(channel.id)] = copy.deepcopy( self._state_dict['demo'] )
         self._state_dict[str(channel.id)]['label_name'] = label_name
+    
+    @abstractmethod
+    def update_log_path(self):
+        self._path_log_book = ''
 
     @abstractmethod
     async def add_item(self):
@@ -34,11 +39,20 @@ class fundamental_state(ABC):
     async def update_json(self):
         with open(self._json_path , 'r', encoding='utf-8') as jsonfile:
             self._state_dict = json.load(jsonfile)
-
+    async def output_logbook(self, typee:str ,text = 'none'):
+        timing = datetime.datetime.now()  
+        timing_format = timing.strftime("%Y_%m_%d@%H_%M_%S@{}@".format(typee))
+        with open( self._path_log_book,'a+',encoding='utf-8') as output:
+            output.write( timing_format)
+            output.write( text + '\n' )
+        pass
+    async def get_channelinfo_dict(self,channel_id:str):
+        return self._state_dict[channel_id]
 class manga_state(fundamental_state):
     def __init__(self, path):
         fundamental_state.__init__(self,path)
 
+        self.update_log_path()
     async def add_item(self,channel:dc_channel, web_list:list):
         channel_id = str( channel.id )
         if( channel_id  not in self.get_channels_dict().keys() ):
@@ -48,18 +62,19 @@ class manga_state(fundamental_state):
             solu = check_manga_state(web,'_')
             self._state_dict[channel_id]['manga'][web]['title']=solu._title
             self._state_dict[channel_id]['manga'][web]['dep'] = solu._words[1]
-    async def get_manga_dict(self,channel_id:str):
-        return self._state_dict[channel_id]
+
     async def update_manga_info(self,channel_id:str, updated_manga:manga_updated):
         self._state_dict[channel_id]['manga'][updated_manga._web]['dep'] = updated_manga._words[1]
         self._state_dict[channel_id]['manga'][updated_manga._web]['title']= updated_manga._title
+    def update_log_path(self):
+        self._path_log_book = './data/log_book/manga_output.md'
 class chicken_state(fundamental_state):
     def __init__(self, path):
         fundamental_state.__init__(self,path)
 
         self.soup_path = './pkg/chicken_soup.json'
         self.update_soup_json()
-
+        self.update_log_path()
     async def add_item(self,channel:dc_channel, people_list:list):
         channel_id = str( channel.id )
         channel_infos = await self.get_channels_dict()
@@ -77,3 +92,5 @@ class chicken_state(fundamental_state):
     def update_soup_json(self):
         with open( self.soup_path , 'r', encoding='utf-8') as jsonfile:
             self._soup_dict = json.load(jsonfile)
+    def update_log_path(self):
+        self._path_log_book = './data/log_book/chicken_output.md'
